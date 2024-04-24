cc78c998fe0341a8535959725b1a4d0461970688
Khang Nguyen <khangnguyen@microsoft.com>
2024-04-24 13:56:40 -0700

    nit space

--- symcrypt-bindgen/build.rs
+++ symcrypt-bindgen/build.rs
@@ -58,6 +58,7 @@ fn main() {
         .derive_default(true)
         .generate()
         .expect("Unable to generate bindings");
+    
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("raw_generated_bindings.rs"))
