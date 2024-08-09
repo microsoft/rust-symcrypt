2892ec4a5630812da3aab86ca9b03aa22c8b0061
Khang Nguyen <khangnguyen@microsoft.com>
2024-08-09 13:03:45 -0700

    Running cargo fmt

--- rust-symcrypt/src/ecc/mod.rs
+++ rust-symcrypt/src/ecc/mod.rs
@@ -1,68 +1,68 @@
 //! ECC functions related to creating an EcKey. For further information please see symcrypt.h for more info
-//! 
+//!
 //! This module provides a way to create and use ECC keys that can be used for both ECDH and ECDSA operations.
-//! 
+//!
 //! The [`EcKey`] struct is a wrapper around SymCrypt's ECC key object and can hold either just an ECC public key, or an ECC key pair.
 //! From the [`EcKey`] object you can preform either EcDsa sign and verify or EcDh secret agreement operations.
-//! 
+//!
 //! For more information on [`ecdsa`] or [`ecdh`] please refer to the respective modules.
-//! 
-//! # Examples 
-//! 
+//!
+//! # Examples
+//!
 //! ## Generate a new EcKey pair for EcDsa usage
 //! ```rust
 //! use symcrypt::ecc::{EcKey, CurveType, EcKeyUsage};
-//! 
+//!
 //! // Generate a random key pair with the Curve P256, and with the intended usage of the key to be EcDsa.
 //! let key = EcKey::generate_key_pair(CurveType::NistP256, EcKeyUsage::EcDsa).unwrap();
-//! 
+//!
 //! // Get attributes of the key
-//! let public_key_size = key.get_size_of_public_key(); 
+//! let public_key_size = key.get_size_of_public_key();
 //! let private_key_size = key.get_size_of_private_key(); // will be 0.
 //! let curve_type = key.get_curve_type();
 //! let ec_usage = key.get_ec_curve_usage();
 //!
 //! // Assert that this key is a key pair.
 //! assert!(key.has_private_key());
-//! 
+//!
 //! // key can now be used for EcDsa sign and verify operations.
-//! 
+//!
 //! // Export the public key
 //! let public_key = key.export_public_key().unwrap();
 //! ```
-//! 
+//!
 //! ## Set a key pair for EcDh usage
 //! ```rust
 //! use symcrypt::ecc::{EcKey, CurveType, EcKeyUsage};
 //! use hex::*;
-//! 
+//!
 //! // NistP256 sample key
 //! let private_key = decode("b20d705d9bd7c2b8dc60393a5357f632990e599a0975573ac67fd89b49187906").unwrap();
-//! 
+//!
 //! // Set a key pair with the Curve P256, and with the intended usage of the key to be EcDh.
 //! // Setting `None` for the public_key parameter will automatically generate the public key based on the private key bytes.
 //! let key = EcKey::set_key_pair(CurveType::NistP256, &private_key, None, EcKeyUsage::EcDh).unwrap();
-//! 
+//!
 //! // Get attributes of the key
-//! let public_key_size = key.get_size_of_public_key(); 
+//! let public_key_size = key.get_size_of_public_key();
 //! let private_key_size = key.get_size_of_private_key();
 //! let curve_type = key.get_curve_type();
 //! let ec_usage = key.get_ec_curve_usage();
 //!
 //! // Assert that this key is a key pair.
 //! assert!(key.has_private_key());
-//! 
+//!
 //! // Export the public key
 //! let public_key = key.export_public_key().unwrap();
 //! ```
-//! 
+//!
 use crate::NumberFormat;
 use crate::{errors::SymCryptError, symcrypt_init};
 use lazy_static::lazy_static;
 use std::ptr;
 
-pub mod ecdsa;
 pub mod ecdh;
+pub mod ecdsa;
 
 #[derive(Copy, Clone, PartialEq, Debug)]
 /// [`CurveType`] provides an enum of the curve types that can be used when creating a key(s)
@@ -75,7 +75,7 @@ pub enum CurveType {
 
 #[derive(Debug)]
 // EcKey is a wrapper around symcrypt_sys::PSYMCRYPT_ECKEY.
-pub(crate) struct InnerEcKey (symcrypt_sys::PSYMCRYPT_ECKEY);
+pub(crate) struct InnerEcKey(symcrypt_sys::PSYMCRYPT_ECKEY);
 
 // Must drop the EcKey before the expanded EcCurve is dropped
 // EcCurve has static lifetime so this will always be the case.
@@ -128,21 +128,23 @@ impl EcKeyUsage {
         match self {
             EcKeyUsage::EcDsa => symcrypt_sys::SYMCRYPT_FLAG_ECKEY_ECDSA,
             EcKeyUsage::EcDh => symcrypt_sys::SYMCRYPT_FLAG_ECKEY_ECDH,
-            EcKeyUsage::EcDhAndEcDsa =>  { symcrypt_sys::SYMCRYPT_FLAG_ECKEY_ECDSA | symcrypt_sys::SYMCRYPT_FLAG_ECKEY_ECDH },
+            EcKeyUsage::EcDhAndEcDsa => {
+                symcrypt_sys::SYMCRYPT_FLAG_ECKEY_ECDSA | symcrypt_sys::SYMCRYPT_FLAG_ECKEY_ECDH
+            }
         }
     }
 }
 
 #[derive(Debug)]
 /// ECC Key State Object.
-/// 
+///
 /// `EcKey` is a struct that represents an Ec Key. It can hold either just an Ec public key, or an Ec key pair.
 /// To check if if the key has a private key attached, you can use [`EcKey::has_private_key()`]
 pub struct EcKey {
     inner_key: InnerEcKey,
     curve_type: CurveType,
     ec_key_usage: EcKeyUsage,
-    has_private_key: bool
+    has_private_key: bool,
 }
 
 unsafe impl Send for EcKey {
@@ -155,13 +157,16 @@ unsafe impl Sync for EcKey {
 
 impl EcKey {
     /// `generate_key_pair()` generates a random Ec Key object that has a public and private key attached based on the [`CurveType`] and [`EcKeyUsage`] provided.
-    /// 
+    ///
     /// `curve_type` represents the [`CurveType`] of the key.
-    /// 
+    ///
     /// `ec_key_usage` represents the [`EcKeyUsage`] of the key and indicates if the key will be used for [`EcKeyUsage::EcDsa`] or [`EcKeyUsage::EcDh`], or [`EcKeyUsage::EcDhAndEcDsa`].
-    /// 
+    ///
     /// This function will return a [`SymCryptError::MemoryAllocationFailure`] if there is not enough memory to allocate the key.
-    pub fn generate_key_pair(curve_type: CurveType, ec_key_usage: EcKeyUsage) -> Result<Self, SymCryptError> {
+    pub fn generate_key_pair(
+        curve_type: CurveType,
+        ec_key_usage: EcKeyUsage,
+    ) -> Result<Self, SymCryptError> {
         let ec_curve = InnerEcCurve::new(curve_type); // Can fail here due to insufficient memory.
         unsafe {
             // SAFETY: FFI calls
@@ -171,16 +176,13 @@ impl EcKey {
                 return Err(SymCryptError::MemoryAllocationFailure);
             }
 
-            match symcrypt_sys::SymCryptEckeySetRandom(
-                ec_key_usage.to_symcrypt_flag(),
-                key_ptr.0
-            ) {
+            match symcrypt_sys::SymCryptEckeySetRandom(ec_key_usage.to_symcrypt_flag(), key_ptr.0) {
                 symcrypt_sys::SYMCRYPT_ERROR_SYMCRYPT_NO_ERROR => {
                     let key = EcKey {
                         inner_key: key_ptr,
                         curve_type: curve_type,
                         ec_key_usage,
-                        has_private_key: true
+                        has_private_key: true,
                     };
                     Ok(key)
                 }
@@ -189,21 +191,25 @@ impl EcKey {
         }
     }
 
-
     /// `set_key_pair()` sets the private and public key information onto the [`EcKey`].
-    /// 
+    ///
     /// `curve_type` represents the [`CurveType`] of the key.
-    /// 
+    ///
     /// `private_key` is a `&[u8]` that represents the private key. This key type must match that of the curve type and will fail with [`SymCryptError::InvalidArgument`] if it does not.
-    /// 
+    ///
     /// `public_key` is an optional `&[u8]` that represents the public key. If no public key is provided, the public key will be derived from the private key. If a public key is provided,
     /// it must match the format of the private key and will fail with [`SymCryptError::InvalidArgument`] if it does not.
-    /// 
+    ///
     /// `ec_key_usage` represents the [`EcKeyUsage`] of the key and indicates if the key will be used for [`EcKeyUsage::EcDsa`] or [`EcKeyUsage::EcDh`], or [`EcKeyUsage::EcDhAndEcDsa`].
-    pub fn set_key_pair(curve_type: CurveType, private_key: &[u8], public_key: Option<&[u8]>, ec_key_usage: EcKeyUsage) -> Result<Self, SymCryptError> {
+    pub fn set_key_pair(
+        curve_type: CurveType,
+        private_key: &[u8],
+        public_key: Option<&[u8]>,
+        ec_key_usage: EcKeyUsage,
+    ) -> Result<Self, SymCryptError> {
         let ec_curve = InnerEcCurve::new(curve_type); // Can fail here due to insufficient memory.
 
-        unsafe{ 
+        unsafe {
             //SAFETY: FFI calls
             // Stack allocated since we will do SymCryptEckeyAllocate.
             let key_ptr = InnerEcKey(symcrypt_sys::SymCryptEckeyAllocate(ec_curve.0));
@@ -225,14 +231,14 @@ impl EcKey {
                 curve_to_num_format(curve_type),
                 curve_to_ec_point_format(curve_type),
                 ec_key_usage.to_symcrypt_flag(),
-                key_ptr.0
+                key_ptr.0,
             ) {
                 symcrypt_sys::SYMCRYPT_ERROR_SYMCRYPT_NO_ERROR => {
                     let key = EcKey {
                         inner_key: key_ptr,
                         curve_type: curve_type,
                         ec_key_usage,
-                        has_private_key: true
+                        has_private_key: true,
                     };
                     Ok(key)
                 }
@@ -242,15 +248,19 @@ impl EcKey {
     }
 
     /// `set_public_key()` sets the public key information onto the [`EcKey`].
-    /// 
+    ///
     /// `curve_type` represents the [`CurveType`] of the key.
-    /// 
+    ///
     /// `public_key` is a `&[u8]` that represents the public key. This key type must match that of the curve type and will fail with [`SymCryptError::InvalidArgument`] if it does not.
-    /// 
+    ///
     /// `ec_key_usage` represents the [`EcKeyUsage`] of the key and indicates if the key will be used for [`EcKeyUsage::EcDsa`] or [`EcKeyUsage::EcDh`], or [`EcKeyUsage::EcDhAndEcDsa`].
-    /// 
+    ///
     ///  This function will return a [`SymCryptError::MemoryAllocationFailure`] if there is not enough memory to allocate the key.
-    pub fn set_public_key(curve_type: CurveType, public_key: &[u8], ec_key_usage: EcKeyUsage) -> Result<Self, SymCryptError> {
+    pub fn set_public_key(
+        curve_type: CurveType,
+        public_key: &[u8],
+        ec_key_usage: EcKeyUsage,
+    ) -> Result<Self, SymCryptError> {
         let ec_curve = InnerEcCurve::new(curve_type); // Can fail here due to insufficient memory.
         unsafe {
             // SAFETY: FFI calls
@@ -275,7 +285,7 @@ impl EcKey {
                         inner_key: key_ptr,
                         curve_type: curve_type,
                         ec_key_usage,
-                        has_private_key: false
+                        has_private_key: false,
                     };
                     Ok(key)
                 }
@@ -323,7 +333,7 @@ impl EcKey {
         self.curve_type
     }
 
-    /// `get_ec_curve_usage()` returns the [`EcKeyUsage`] associated with the [`EcKey`]. 
+    /// `get_ec_curve_usage()` returns the [`EcKeyUsage`] associated with the [`EcKey`].
     /// This call cannot fail.
     pub fn get_ec_curve_usage(&self) -> EcKeyUsage {
         self.ec_key_usage
@@ -345,7 +355,7 @@ impl EcKey {
         }
     }
 
-    /// `get_size_of_private_key()` returns `u32` that represents the size of the private key associated with the [`EcKey`]. 
+    /// `get_size_of_private_key()` returns `u32` that represents the size of the private key associated with the [`EcKey`].
     /// this call cannot fail.
     pub fn get_size_of_private_key(&self) -> u32 {
         unsafe {
@@ -359,7 +369,10 @@ impl EcKey {
     pub fn get_size_of_public_key(&self) -> u32 {
         unsafe {
             // SAFETY: FFI calls
-            symcrypt_sys::SymCryptEckeySizeofPublicKey(self.inner_key(), curve_to_ec_point_format(self.get_curve_type()))
+            symcrypt_sys::SymCryptEckeySizeofPublicKey(
+                self.inner_key(),
+                curve_to_ec_point_format(self.get_curve_type()),
+            )
         }
     }
 }
@@ -432,7 +445,6 @@ pub(crate) fn curve_to_ec_point_format(curve_type: CurveType) -> i32 {
     ec_point_format
 }
 
-
 #[cfg(test)]
 mod test {
     use super::*;
@@ -456,15 +468,23 @@ mod test {
 
     #[test]
     fn test_eckey_set_key_pair() {
-
-        let private_key = hex::decode("b20d705d9bd7c2b8dc60393a5357f632990e599a0975573ac67fd89b49187906").unwrap();
+        let private_key =
+            hex::decode("b20d705d9bd7c2b8dc60393a5357f632990e599a0975573ac67fd89b49187906")
+                .unwrap();
         let public_key = hex::decode("51f99d2d52d4a6e734484a018b7ca2f895c2929b6754a3a03224d07ae61166ce4737da963c6ef7247fb88d19f9b0c667cac7fe12837fdab88c66f10d3c14cad1").unwrap();
 
-        let key = EcKey::set_key_pair(CurveType::NistP256, &private_key, Some(&public_key), EcKeyUsage::EcDsa).unwrap();
+        let key = EcKey::set_key_pair(
+            CurveType::NistP256,
+            &private_key,
+            Some(&public_key),
+            EcKeyUsage::EcDsa,
+        )
+        .unwrap();
         assert_eq!(key.get_curve_type(), CurveType::NistP256);
         assert_eq!(key.has_private_key(), true);
 
-        let key2 = EcKey::set_key_pair(CurveType::NistP256, &private_key, None, EcKeyUsage::EcDsa).unwrap();
+        let key2 = EcKey::set_key_pair(CurveType::NistP256, &private_key, None, EcKeyUsage::EcDsa)
+            .unwrap();
 
         assert_eq!(key2.export_public_key(), key.export_public_key());
     }
@@ -472,34 +492,59 @@ mod test {
     #[test]
     fn test_eckey_set_key_wrong_curve_type() {
         // NistP256 Key
-        let private_key = hex::decode("b20d705d9bd7c2b8dc60393a5357f632990e599a0975573ac67fd89b49187906").unwrap();
+        let private_key =
+            hex::decode("b20d705d9bd7c2b8dc60393a5357f632990e599a0975573ac67fd89b49187906")
+                .unwrap();
 
-        let result = EcKey::set_key_pair(CurveType::NistP384, &private_key, None, EcKeyUsage::EcDsa).unwrap_err();
+        let result =
+            EcKey::set_key_pair(CurveType::NistP384, &private_key, None, EcKeyUsage::EcDsa)
+                .unwrap_err();
 
         assert_eq!(result, SymCryptError::InvalidArgument);
-
     }
 
     #[test]
     fn test_eckey_set_key_ecdh_and_ecdsa() {
         // NistP256 Key
-        let private_key = hex::decode("b20d705d9bd7c2b8dc60393a5357f632990e599a0975573ac67fd89b49187906").unwrap();
+        let private_key =
+            hex::decode("b20d705d9bd7c2b8dc60393a5357f632990e599a0975573ac67fd89b49187906")
+                .unwrap();
 
-        let key = EcKey::set_key_pair(CurveType::NistP256, &private_key, None, EcKeyUsage::EcDhAndEcDsa).unwrap();
+        let key = EcKey::set_key_pair(
+            CurveType::NistP256,
+            &private_key,
+            None,
+            EcKeyUsage::EcDhAndEcDsa,
+        )
+        .unwrap();
 
         assert_eq!(key.get_ec_curve_usage(), EcKeyUsage::EcDhAndEcDsa);
     }
 
     #[test]
     fn test_eckey_set_key_pair_invalid_key_size() {
-        let result = EcKey::set_key_pair(CurveType::NistP256, &[0u8; 3], Some(&[0u8; 99]), EcKeyUsage::EcDsa).unwrap_err();
+        let result = EcKey::set_key_pair(
+            CurveType::NistP256,
+            &[0u8; 3],
+            Some(&[0u8; 99]),
+            EcKeyUsage::EcDsa,
+        )
+        .unwrap_err();
         assert_eq!(result, SymCryptError::InvalidArgument);
     }
 
     #[test]
     fn test_eckey_set_key_pair_invalid_public_key() {
-        let private_key = hex::decode("b20d705d9bd7c2b8dc60393a5357f632990e599a0975573ac67fd89b49187906").unwrap();
-        let result = EcKey::set_key_pair(CurveType::NistP256, &private_key, Some(&[0u8; 64]), EcKeyUsage::EcDsa).unwrap_err();
+        let private_key =
+            hex::decode("b20d705d9bd7c2b8dc60393a5357f632990e599a0975573ac67fd89b49187906")
+                .unwrap();
+        let result = EcKey::set_key_pair(
+            CurveType::NistP256,
+            &private_key,
+            Some(&[0u8; 64]),
+            EcKeyUsage::EcDsa,
+        )
+        .unwrap_err();
         assert_eq!(result, SymCryptError::InvalidArgument);
     }
 
@@ -507,7 +552,8 @@ mod test {
     fn test_eckey_generate_and_set_public_key() {
         let key = EcKey::generate_key_pair(CurveType::NistP256, EcKeyUsage::EcDsa).unwrap();
         let public_key = key.export_public_key().unwrap();
-        let key2 = EcKey::set_public_key(CurveType::NistP256, &public_key, EcKeyUsage::EcDsa).unwrap();
+        let key2 =
+            EcKey::set_public_key(CurveType::NistP256, &public_key, EcKeyUsage::EcDsa).unwrap();
         assert_eq!(key.get_curve_type(), CurveType::NistP256);
         assert_eq!(key2.get_curve_type(), CurveType::NistP256);
         assert_eq!(key.has_private_key(), true);
@@ -527,28 +573,31 @@ mod test {
     }
 
     #[test]
-    fn test_eckey_set_public_key() { 
+    fn test_eckey_set_public_key() {
         let public_key = hex::decode("8bcfe2a721ca6d753968f564ec4315be4857e28bef1908f61a366b1f03c974790f67576a30b8e20d4232d8530b52fb4c89cbc589ede291e499ddd15fe870ab96").unwrap();
 
-        let key = EcKey::set_public_key(CurveType::NistP256, &public_key, EcKeyUsage::EcDsa).unwrap();
+        let key =
+            EcKey::set_public_key(CurveType::NistP256, &public_key, EcKeyUsage::EcDsa).unwrap();
         assert_eq!(key.get_curve_type(), CurveType::NistP256);
         assert_eq!(key.has_private_key(), false);
         assert_eq!(key.inner_key(), key.inner_key());
     }
 
     #[test]
-    fn test_eckey_set_public_key_wrong_curve_curve_25519() { 
+    fn test_eckey_set_public_key_wrong_curve_curve_25519() {
         let public_key = hex::decode("8bcfe2a721ca6d753968f564ec4315be4857e28bef1908f61a366b1f03c974790f67576a30b8e20d4232d8530b52fb4c89cbc589ede291e499ddd15fe870ab96").unwrap();
 
-        let result = EcKey::set_public_key(CurveType::Curve25519, &public_key, EcKeyUsage::EcDsa).unwrap_err();
+        let result = EcKey::set_public_key(CurveType::Curve25519, &public_key, EcKeyUsage::EcDsa)
+            .unwrap_err();
         assert_eq!(result, SymCryptError::InvalidArgument);
     }
 
     #[test]
-    fn test_eckey_set_public_key_wrong_curve_nist_384() { 
+    fn test_eckey_set_public_key_wrong_curve_nist_384() {
         let public_key = hex::decode("8bcfe2a721ca6d753968f564ec4315be4857e28bef1908f61a366b1f03c974790f67576a30b8e20d4232d8530b52fb4c89cbc589ede291e499ddd15fe870ab96").unwrap();
 
-        let result = EcKey::set_public_key(CurveType::NistP384, &public_key, EcKeyUsage::EcDsa).unwrap_err();
+        let result =
+            EcKey::set_public_key(CurveType::NistP384, &public_key, EcKeyUsage::EcDsa).unwrap_err();
         assert_eq!(result, SymCryptError::InvalidArgument);
     }
 
