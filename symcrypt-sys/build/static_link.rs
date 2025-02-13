3f7afcbf059315cd23770a3ed5322db5559d25a6
Khang Nguyen <khangnguyen@microsoft.com>
2025-02-13 15:05:14 -0500

    removing c11 check

--- symcrypt-sys/build/static_link.rs
+++ symcrypt-sys/build/static_link.rs
@@ -77,16 +77,7 @@ impl SymCryptOptions {
         if !self.symcrypt_use_asm {
             cc.define("SYMCRYPT_IGNORE_PLATFORM", None); // TODO: Fix when we get ASM
         }
-
-        // Set C11 standard for Windows and GNU targets.
-        match self.triple {
-            Triple::x86_64_pc_windows_msvc | Triple::aarch64_pc_windows_msvc => {
-                cc.flag("-std=c11");
-            }
-            Triple::aarch64_unknown_linux_gnu | Triple::x86_64_unknown_linux_gnu => {
-                cc.flag("-std=gnu11");
-            }
-        }
+        
         // Set specific flags for each target
         match self.triple {
             Triple::x86_64_pc_windows_msvc => {
