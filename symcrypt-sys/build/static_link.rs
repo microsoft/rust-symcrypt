d1ea4036b32c4ed387816910a5b0d128bbbd4d2f
Maxwell Moyer-McKee <mamckee@microsoft.com>
2025-04-01 00:55:14 +0000

    Use command like for pre-processing. Generate buildinfo

--- symcrypt-sys/build/static_link.rs
+++ symcrypt-sys/build/static_link.rs
@@ -1,6 +1,7 @@
 use super::triple::Triple;
-use std::{collections::HashSet};
+use std::{collections::HashSet, process::Command};
 use std::io::{self, Write};
+use chrono::{DateTime, SecondsFormat, Utc};
 
 const LIB_NAME: &str = "symcrypt";
 
@@ -295,6 +296,9 @@ xmss.c
 xtsaes.c
 ";
 
+const BUILD_INFO_TEMPLATE: &str = "symcrypt/conf/buildInfo.h.in";
+const BUILD_INFO_OUT: &str = "symcrypt/inc/buildInfo.h";
+
 const AMD64_WINDOWS_CPPASM_SOURCES: &str = "
 aesasm-masm.cppasm
 fdef_asm-masm.cppasm
@@ -331,62 +335,171 @@ fdef369_asm-gas.cppasm
 wipe-gas.cppasm
 ";
 
-fn process_symcrypt_cppasm(cppasm_sources: &str, arch: &str, is_windows: bool, source_files: &mut Vec<String>) -> std::io::Result<()> {
-    // Prepares list of files to be compiled
-    let cppasm_files: Vec<& str> = cppasm_sources
-        .lines()
-        .map(str::trim) // Trim once instead of inside filter
-        .filter(|line| {
-            !line.is_empty() && !line.starts_with("#")
-        })
-        .collect();    
+fn generate_build_info() -> std::io::Result<()> {
+    // Run git commands to get the current branch, commit, and timestamp
+    let symcrypt_git_branch = match String::from_utf8(Command::new("git")
+        .current_dir("symcrypt")
+        .arg("branch")
+        .arg("--show")
+        .output()?
+        .stdout)
+    {
+        Ok(symcrypt_git_branch) => symcrypt_git_branch,
+        Err(_) => {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "Failed to convert timestamp to string",
+            ));
+        }
+    };
+
+    let symcrypt_git_commit = match String::from_utf8(Command::new("git")
+        .current_dir("symcrypt")
+        .arg("log")
+        .arg("-1")
+        .arg("--format=%h")
+        .output()?
+        .stdout)
+    {
+        Ok(symcrypt_git_commit) => symcrypt_git_commit,
+        Err(_) => {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "Failed to convert timestamp to string",
+            ));
+        }
+    };
+
+    let symcrypt_commit_timestamp_raw = match String::from_utf8(Command::new("git")
+        .current_dir("symcrypt")
+        .arg("log")
+        .arg("-1")
+        .arg("--date=iso-strict-local")
+        .arg("--format=%cd")
+        .output()?
+        .stdout)
+    {
+        Ok(symcrypt_git_timestamp_raw) => symcrypt_git_timestamp_raw,
+        Err(_) => {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "Failed to convert timestamp to string",
+            ));
+        }
+    };
+
+    let symcrypt_commit_datetime = match DateTime::parse_from_rfc3339(symcrypt_commit_timestamp_raw.trim()) {
+        Ok(symcrypt_commit_datetime) => symcrypt_commit_datetime,
+        Err(_) => {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                format!("Failed to parse git timestamp: {}", symcrypt_commit_timestamp_raw.trim()),
+            ));
+        }
+    };
+
+    let build_timestamp_formatted= Utc::now().to_rfc3339_opts(SecondsFormat::Secs, true);
+    let build_timestamp = match build_timestamp_formatted.strip_suffix("Z") {
+        Some(build_timestamp) => build_timestamp,
+        None => {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                "Failed to convert UTC build timestamp to string",
+            ));
+        }
+    };
+
+
+    // Open the build info template and replace the placeholders with the actual values
+    let build_info_generated = std::fs::read_to_string(BUILD_INFO_TEMPLATE)?
+        .replace("@SYMCRYPT_BUILD_INFO_BRANCH@", symcrypt_git_branch.trim())
+        .replace("@SYMCRYPT_BUILD_INFO_COMMIT@", format!("{}_{}", symcrypt_commit_datetime.to_rfc3339(), symcrypt_git_commit.trim()).as_str())
+        .replace("@SYMCRYPT_BUILD_INFO_TIMESTAMP@", build_timestamp);
+
+    // Write the generated build info to the output file
+    std::fs::File::create(BUILD_INFO_OUT)?
+        .write_all(build_info_generated.as_bytes())
+}
+
+fn process_symcrypt_cppasm(options: &SymCryptOptions, source_files: &mut Vec<String>) -> std::io::Result<()> {
+    let cppasm_files: Vec<&str>= match options.triple() {
+        Triple::x86_64_pc_windows_msvc => AMD64_WINDOWS_CPPASM_SOURCES,        
+        Triple::aarch64_pc_windows_msvc => ARM64_WINDOWS_CPPASM_SOURCES,
+        Triple::x86_64_unknown_linux_gnu => AMD64_LINUX_CPPASM_SOURCES,
+        Triple::aarch64_unknown_linux_gnu => ARM64_LINUX_CPPASM_SOURCES,
+    }
+    .lines()
+    .map(str::trim) // Trim once instead of inside filter
+    .filter(|line| {
+        !line.is_empty() && !line.starts_with("#")
+    })
+    .collect();
+
+    let (arch, arch_upper) = match options.triple() {
+        Triple::x86_64_pc_windows_msvc | Triple::x86_64_unknown_linux_gnu => ("amd64", "AMD64"),
+        Triple::aarch64_pc_windows_msvc | Triple::aarch64_unknown_linux_gnu => ("arm64", "ARM64"),
+    };
+
+    let compiler  = match cc::Build::new().try_get_compiler() {
+        Ok(tool) => tool.to_command(),
+        Err(e) => {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                format!("Failed to get compiler: {}", e),
+            ));
+        }
+    };
 
     for cppasm_file in cppasm_files {
         // Get the file name without extension
         let asm_out = format!("{arch}/{}", cppasm_file.replace("cppasm", "asm"));
-        let bytes: Vec<u8>;
-        let mut cc = cc::Build::new();
-        cc.file(format!("{SOURCE_DIR}/{arch}/linux_gnu/{cppasm_file}"))
-            .include(format!("symcrypt/inc"))
-            .include(format!("{SOURCE_DIR}"))
-            .include(format!("{SOURCE_DIR}/{arch}"))
-            .define(format!("SYMCRYPT_CPU_{}", arch.to_uppercase()).as_str(), None);
-
-        if is_windows
-        {
-            cc.flag("/nologo")
-                .flag("/P")
-                .define("-DSYMCRYPT_MASM", None);
-        } else {     
-            cc.flag("-P")
-                .flag("-xc")
-                .define("SYMCRYPT_GAS", None);
-        }
-
-        match cc.try_expand() {
-            Ok(out) => {
-                bytes = out;
-            }
-            Err(e) => {
-                return Err(io::Error::new(
-                    io::ErrorKind::Other,
-                    format!("Failed to preprocess {cppasm_file}. cc exited with: {}", e),
-                ));
-            }
-        }
+        let output = match options.triple() {
+            Triple::x86_64_pc_windows_msvc | Triple::aarch64_pc_windows_msvc =>
+                Command::new(compiler.get_program()).arg("/nologo")
+                    .arg("/EP")
+                    .arg("/P")
+                    .arg(format!("/Fi{SOURCE_DIR}/{asm_out}"))
+                    .arg(format!("{SOURCE_DIR}/{arch}/windows_msvc/{cppasm_file}"))
+                    .arg(format!("-Isymcrypt/inc"))
+                    .arg(format!("-I{SOURCE_DIR}"))
+                    .arg(format!("-I{SOURCE_DIR}/{arch}"))
+                    .arg(format!("-DSYMCRYPT_MASM"))
+                    .arg(format!("-DSYMCRYPT_CPU_{arch_upper}"))
+                    .output()?,
+            Triple::x86_64_unknown_linux_gnu | Triple::aarch64_unknown_linux_gnu =>
+                Command::new(compiler.get_program()).arg("-E")
+                    .arg("-P")
+                    .arg("-xc")
+                    .arg(format!("{SOURCE_DIR}/{arch}/linux_gnu/{cppasm_file}"))
+                    .arg(format!("-o{SOURCE_DIR}/{asm_out}"))
+                    .arg(format!("-Isymcrypt/inc"))
+                    .arg(format!("-I{SOURCE_DIR}"))
+                    .arg(format!("-I{SOURCE_DIR}/{arch}"))
+                    .arg(format!("-DSYMCRYPT_GAS"))
+                    .arg(format!("-DSYMCRYPT_CPU_{arch_upper}"))
+                    .output()?,
+        };
             
-        let mut asm_fout = std::fs::File::create(format!("{SOURCE_DIR}/{asm_out}"))?;
-        asm_fout.write_all(&bytes)?;
-
-        println!("Preprocessed {cppasm_file} to {asm_out}.");
+        if output.status.success() {
+            println!("Preprocessed {cppasm_file} to {asm_out}: {}", output.status);
+        } else {
+            io::stderr().write_all(&output.stderr)?;
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                format!("Failed to preprocess {cppasm_file}. cc exited with: {}", output.status),
+            ));
+        }
 
         source_files.push(asm_out);
+
     }
 
     Ok(())
 }
 
 fn compile_symcrypt_static(lib_name: &str, options: &SymCryptOptions) -> std::io::Result<()> {
+    generate_build_info()?;
+
     // Compile intermediates required this is currently only required for x86_64_unknown_linux_gnu
     let (already_compiled_files, intermediates) = compile_symcrypt_intermediates(options);
 
@@ -426,21 +539,8 @@ fn compile_symcrypt_static(lib_name: &str, options: &SymCryptOptions) -> std::io
     }
 
     if options.use_asm() {
-        // Preprocess cppasm files to asm
-        match options.triple() {
-            Triple::x86_64_pc_windows_msvc => {
-                process_symcrypt_cppasm(AMD64_WINDOWS_CPPASM_SOURCES, "amd64", true, &mut base_files)?
-            }
-            Triple::aarch64_pc_windows_msvc => {
-                process_symcrypt_cppasm(ARM64_WINDOWS_CPPASM_SOURCES, "arm64", true, &mut base_files)?
-            }
-            Triple::x86_64_unknown_linux_gnu => {
-                process_symcrypt_cppasm(AMD64_LINUX_CPPASM_SOURCES, "amd64", false, &mut base_files)?
-            }
-            Triple::aarch64_unknown_linux_gnu => {
-                process_symcrypt_cppasm(ARM64_LINUX_CPPASM_SOURCES, "arm64", false, &mut base_files)?
-            }
-        };
+        // Preprocess cppasm files to asm and add them to the list of files to be compiled
+        process_symcrypt_cppasm(options, &mut base_files)?;
     }
 
     // Pre-Configure the cc compiler based on the target triple
